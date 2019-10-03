bb349f7c9aebdff07bc4dfe541118bf485c068d2
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-02 21:44:10 -0700

    added Overworld, player movement, & tilemap.

---
+++ UndergroundMiningGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    public float speed = 2.0f;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 tempVect = new Vector2(h, v);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        transform.Translate(tempVect);
+    }
+}
