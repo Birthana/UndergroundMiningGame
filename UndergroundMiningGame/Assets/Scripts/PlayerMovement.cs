691671efe1c32a06ba0e2f916036ed55ac0fedfd
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-03 19:50:28 -0700

    fixed Player Collusion & added Player Animation.

--- UndergroundMiningGame/Assets/Scripts/PlayerMovement.cs
+++ UndergroundMiningGame/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
     public float speed = 2.0f;
+    public Animator animator;
     
     void Start()
     {
@@ -19,8 +20,12 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        animator.SetFloat("horizontal", h);
+        animator.SetFloat("vertical", v);
+
         Vector2 tempVect = new Vector2(h, v);
+        animator.SetFloat("speed", tempVect.sqrMagnitude);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
-        transform.Translate(tempVect);
+        rb.MovePosition(rb.position + tempVect * speed);
     }
 }
