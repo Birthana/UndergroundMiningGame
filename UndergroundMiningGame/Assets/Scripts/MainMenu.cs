377e98d03d14c16533c8cd7473d03c65e37e0110
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-04 18:00:46 -0700

    fixed conflicts by redoing everything.

--- UndergroundMiningGame/Assets/Scripts/MainMenu.cs
+++ UndergroundMiningGame/Assets/Scripts/MainMenu.cs
@@ -5,29 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-<<<<<<< HEAD
-    void Play()
-=======
     public void Play()
->>>>>>> 947e7dbea0087edcce67ee5698fc567dfa24eb45
     {
         SceneManager.LoadScene("Overworld");
     }
-
-<<<<<<< HEAD
-    void Credits()
-=======
     public void Credits()
->>>>>>> 947e7dbea0087edcce67ee5698fc567dfa24eb45
     {
 
     }
 
-<<<<<<< HEAD
-    void Quit()
-=======
     public void Quit()
->>>>>>> 947e7dbea0087edcce67ee5698fc567dfa24eb45
     {
         Application.Quit();
     }
