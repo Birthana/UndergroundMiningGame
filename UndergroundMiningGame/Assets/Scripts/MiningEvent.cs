0dd88615487efae89c05956b65555428f0a23bf7
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-11 17:00:14 -0700

    added Random Mining Events Spawn.

--- UndergroundMiningGame/Assets/Scripts/MiningEvent.cs
+++ UndergroundMiningGame/Assets/Scripts/MiningEvent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MiningEvent : MonoBehaviour
 {
@@ -8,7 +9,15 @@ public class MiningEvent : MonoBehaviour
     {
         if (collision.gameObject.tag.Equals("Player") && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("Mining Game Start");
+            Tilemap tilemap = GetComponent<Tilemap>();
+            Vector3 temp = new Vector3(collision.transform.position.x, collision.transform.position.y + 1, collision.transform.position.z);
+            Vector3Int tilePosition = tilemap.WorldToCell(temp);
+            Tile tile = (Tile) tilemap.GetTile(tilePosition);
+            if (tile.sprite.Equals(GetComponent<RandomMiningEvent>().placeholder))
+            {
+                //tilemap.SetTile(tilePosition, null);
+                Debug.Log("Mining Game Start");
+            }
         }
     }
 }
---
+++ UndergroundMiningGame/Assets/Scripts/RandomMiningEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RandomMiningEvent : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Sprite placeholder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomMiningEventSpawn();
+    }
+
+    public void RandomMiningEventSpawn()
+    {
+        tilemap = GetComponent<Tilemap>();
+        foreach (var position in tilemap.cellBounds.allPositionsWithin)
+        {
+            Vector3Int tilePosition = new Vector3Int(position.x, position.y, position.z);
+            if (tilemap.HasTile(tilePosition))
+            {
+                int rngInt = Random.Range(0, 10);
+                if (rngInt == 0)
+                {
+                    Tile newTile = ScriptableObject.CreateInstance<Tile>();
+                    newTile.sprite = placeholder;
+                    tilemap.SetTile(tilePosition, newTile);
+                    //Debug.Log("Replaced Tile at " + tilePosition);
+                }
+                else
+                {
+                    //Debug.Log("Kept Tile at " + tilePosition);
+                }
+
+            }
+        }
+    }
+}
