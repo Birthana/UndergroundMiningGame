99b6ee662a40ea209d6a727a305e2fd35e27e1a5
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-10-16 00:14:19 -0700

    combined Dialogue System,fixed Tiles,& added NPC.

---
+++ UndergroundMiningGame/Assets/Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject dialogueManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionStay2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            if (collision.gameObject.GetComponent<PlayerMovement>().enabled)
+            {
+                collision.gameObject.GetComponent<PlayerMovement>().enabled = false;
+                dialogueManager.GetComponent<DialogueManager>().StartDialogue(dialogue);
+            }
+            else
+            {
+                dialogueManager.GetComponent<DialogueManager>().DisplayNextSentence();
+            }
+        }
+    }
+}
